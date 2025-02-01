59199c6766900facae57eb4cffb174b6dbae115b
Sangram Shintre <sangramshintre123@gmail.com>
2025-02-01 19:48:54 +0530

    Created Home page using Tailwind CSS

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup' 
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/user-login' element={<UserLogin />} />
+        <Route path='/user-signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<CaptainLogin />} />
+        <Route path = 'captain-signup' element={<CaptainSignup />} />
+      </Routes>
     </div>
   )
 }
 
 export default App
+
